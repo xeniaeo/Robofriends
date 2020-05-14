a445420e52bc7125b0324dfc96a51625f7ccba8b
xeniaeo <linshuyou1030@gmail.com>
2020-05-14 16:31:39 +0800

    Finished project

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Robofriends
+Follow tutorial on Udemy course - React To run the project:
+
+1. Clone this repo
+2. Run npm install
+3. Run npm start
+
+- - -
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,9 @@ import React from 'react';
 // Receiving props = { id, name, email }
 const Card = ({ id, name, email }) => {
 	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/xenia${id}?200x200`} />
+		<div className='bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+			
+			<img alt='robots' src={`https://robohash.org/000${id}?set=set4`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
